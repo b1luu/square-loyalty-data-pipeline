0ec4d1f9fe0079576ffc75eb47d92c670c38dbda
Brandon <b1luu@ucsd.edu>
2026-03-13 12:24:59 -0700

    added cases for empty loyalty_account

--- app/test.py
+++ app/test.py
@@ -10,16 +10,21 @@ client = Square(
 )
 
 cursor = None
-
+count = 0
 while True:
     result = client.loyalty.accounts.search(cursor=cursor)
+
+    if result.loyalty_accounts is None:
+        break
 
     for acct in result.loyalty_accounts:
         loyalty_id = acct.id
         customer_id = acct.customer_id or "NO_CUSTOMER"
+        count += 1 
 
         print(f"loyalty_id:{loyalty_id},customer_id:{customer_id}")
-
+        print(f"processed {count} accounts")
+        
     cursor = result.cursor
 
     if cursor is None:
